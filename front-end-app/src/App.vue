0f16d842831302d37722c3b3adff3f0474febef8
Hung Fan Lin <hlin@nymets.com>
2024-11-09 21:03:54 -0500

    front end

---
+++ front-end-app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="textInput">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      textInput: ''
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
