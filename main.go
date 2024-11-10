37ca5c17e977879243c74396e77482992a1b5847
Hung Fan Lin <hlin@nymets.com>
2024-11-10 11:46:34 -0500

    graphql

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/graphql-go/graphql"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -33,32 +34,77 @@ func mongodb_connect() *mongo.Client {
 	return client
 }
 
-func get_state_names(w http.ResponseWriter, r *http.Request) {
-	client := mongodb_connect()
-	collection = client.Database("states").Collection("state_names")
+var stateType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "State",
+	Fields: graphql.Fields{
+		"name": &graphql.Field{
+			Type: graphql.String,
+		},
+	},
+})
 
-	cursor, err := collection.Find(ctx, bson.M{})
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+var rootQuery = graphql.NewObject(graphql.ObjectConfig{
+	Name: "RootQuery",
+	Fields: graphql.Fields{
+		"states": &graphql.Field{
+			Type: graphql.NewList(stateType),
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				client := mongodb_connect()
+				collection = client.Database("states").Collection("state_names")
+
+				cursor, err := collection.Find(ctx, bson.M{})
+				if err != nil {
+					return nil, err
+				}
+				defer cursor.Close(ctx)
+
+				var stateNames []State
+				for cursor.Next(ctx) {
+					var state State
+					if err := cursor.Decode(&state); err != nil {
+						return nil, err
+					}
+					stateNames = append(stateNames, state)
+				}
+				return stateNames, nil
+			},
+		},
+	},
+})
+
+var schema, _ = graphql.NewSchema(graphql.SchemaConfig{
+	Query: rootQuery,
+})
+
+func executeGraphQL(w http.ResponseWriter, r *http.Request) {
+	var params struct {
+		Query string `json:"query"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&params); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	defer cursor.Close(ctx)
 
-	var state_names []State
-	for cursor.Next(ctx) {
-		var state State
-		if err := cursor.Decode(&state); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		state_names = append(state_names, state)
-	}
+	result := graphql.Do(graphql.Params{
+		Schema:        schema,
+		RequestString: params.Query,
+	})
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(state_names)
+	if result.HasErrors() {
+		log.Println(result.Errors)
+		http.Error(w, "Error executing GraphQL query", http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(result)
 }
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/states", get_state_names).Methods("GET")
+	router.HandleFunc("/graphql", executeGraphQL).Methods("POST")
+
+	log.Println("Server starting on :8080...")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
